37d02b9f79a0ecb37ddf0833909d402104e6d154
Eric Yang <eyang10000@gmail.com>
2023-09-25 22:47:33 -0400

    add crosshair

--- web/components/svg/ConnectionGrid.jsx
+++ web/components/svg/ConnectionGrid.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState, useRef, useCallback } from 'react';
+import styles from './grid.module.css';
 import Resistor from './circuit/resistor';
 import OpAmp from './circuit/opamp';
 import Capacitor from './circuit/capacitor';
@@ -243,7 +244,7 @@ export default function ConnectionGrid() {
         <circle id="pattern-circle" cx="20" cy="20" r="2" fill="#000"></circle>
       </pattern>
       <g id="view" transform={`translate(${windowSize.x / 2-center.x},${windowSize.y / 2-center.y})`} 
-        onMouseDown={onMouseDown} onMouseMove={onMouseMove} onMouseUp={onMouseUp}>
+        onMouseDown={onMouseDown} onMouseMove={onMouseMove} onMouseUp={onMouseUp} className={styles[mode]}>
         <circle id="origin" cx="0" cy="0" r="5" fill="#f00"></circle>
         <rect id="rect" x={center.x-windowSize.x / 2} y={center.y-windowSize.y / 2} width="100%" height="100%" fill="url(#pattern-circles)"></rect>
         <g>
