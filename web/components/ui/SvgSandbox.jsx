abe44d9a448488fc47b32ed2430da8db00e2803c
Eric Yang <eyang10000@gmail.com>
2023-09-27 18:30:30 -0400

    add ability to clear text window

--- web/components/ui/SvgSandbox.jsx
+++ web/components/ui/SvgSandbox.jsx
@@ -29,10 +29,11 @@ export default function SvgSandbox(props) {
 
   return <div className={styles.sandbox}>
     <div className={styles.textbox} style={{display: (textBuffer[index] === "") ? "none" : "flex"}}>
-      <p className={styles.text}>{textBuffer[Math.min(index, textBuffer.length-1)]}</p>
+      <p className={styles.text} style={{marginTop: '5vmin'}}>{textBuffer[Math.min(index, textBuffer.length-1)]}</p>
       <p style={{position: 'absolute', bottom: '1vmin', width: '100%', textAlign: 'center'}}>{index} of {textBuffer.length-1}</p>
       <button onClick={back} style={{position: 'absolute', bottom: '1vmin', left: '1vmin', width: '3vmin', height: '3vmin'}} type="button">&lt;</button>
       <button onClick={forward} style={{position: 'absolute', bottom: '1vmin', right: '1vmin', width: '3vmin', height: '3vmin'}} type="button">&gt;</button>
+      <button onClick={clearBuffer} style={{position: 'absolute', top: '1vmin', right: '1vmin', width: '3vmin', height: '3vmin'}} type="button">x</button>
     </div>
     <svg width="100%" height="100%" style={{position: 'absolute', top: 0, left: 0}}>
       <ConnectionGrid setText={bufferText} clearBuffer={clearBuffer} {...props}/>
